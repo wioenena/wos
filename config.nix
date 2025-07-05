d0e87ace8bdb3bd2ac8f2921e957d82e3b91722e
wioenena <wioenena@gmail.com>
2025-07-05 21:38:21 +0300

    .

--- config.nix
+++ config.nix
@@ -17,6 +17,7 @@
     pkg:
     builtins.elem (lib.getName pkg) [
       "spotify"
+      "vscode"
     ];
 
   boot = {
--- users/wioenena/programs/vscode/default.nix
+++ users/wioenena/programs/vscode/default.nix
@@ -1,5 +1,37 @@
-{...}:{
-programs.vscode = {
-enable =true;
-};
+{ config, pkgs, ... }:
+{
+  programs.vscode = {
+    enable = true;
+
+    profiles.${config.home.username} = {
+      userSettings = {
+        window = {
+          titleBarStyle = "custom";
+          zoomLevel = 0;
+          customTitleBarVisibility = "auto";
+        };
+
+        editor = {
+          fontSize = 16;
+          cursorStyle = "block";
+          cursorBlinking = "smooth";
+          cursorSmoothCaretAnimation = "on";
+          minimap.autohide = true;
+          wordWrap = "on";
+
+        };
+
+        workbench.activityBar.location = "top";
+      };
+
+      extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "cpptools";
+          publisher = "ms-vscode";
+          version = "1.26.3";
+          sha256 = "sha256-f+7GTLxeBVUxuICbJm5H6pjNh1jzqq4x/Mi+Oje8YrU=";
+        }
+      ];
+    };
+  };
 }
