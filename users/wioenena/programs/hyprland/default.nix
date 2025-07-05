1d4a1491bae1407d2fb78e7c40e0287d44a002f8
wioenena <wioenena@gmail.com>
2025-07-06 00:45:30 +0300

    .

--- users/wioenena/programs/hyprland/default.nix
+++ users/wioenena/programs/hyprland/default.nix
@@ -233,6 +233,9 @@ in
 
         # Fix some dragging issues with XWayland
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+
+        # Opacity Zed Editor
+        "opacity 0.9, class:dev.zed.Zed"
       ];
     };
   };
