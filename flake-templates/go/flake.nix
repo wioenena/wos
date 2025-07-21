15e02c40848bfe805bdc296cbc5542ae494472f3
wioenena <wioenena@gmail.com>
2025-07-21 13:19:11 +0300

    .

---
+++ flake-templates/go/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A simple Go package";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+          ];
+        };
+      }
+    );
+
+}
