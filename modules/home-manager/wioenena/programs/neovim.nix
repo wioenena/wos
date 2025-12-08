5920b9b505d1baa0cf048599d507c66d65da4cf0
wioenena <wioenena>
2025-12-08 22:33:01 +0300

    .

---
+++ modules/home-manager/wioenena/programs/neovim.nix
@@ -0,0 +1,31 @@
+{ ... }:
+{
+  programs.neovim = {
+    defaultEditor = true;
+    enable = true;
+    extraLuaConfig = ''
+      vim.o.backup = false
+      vim.o.writebackup = false
+      vim.o.swapfile = false
+      vim.o.ignorecase = true
+      vim.o.smartcase = true
+      vim.o.wrap = true
+      vim.o.numberwidth = 2
+      vim.o.tabstop = 2
+      vim.o.shiftwidth = 2
+      vim.o.smarttab = true
+      vim.o.autoindent = true
+      vim.o.smartindent = true
+      vim.o.foldenable = false
+      vim.o.encoding = "utf-8"
+      vim.o.fileencoding = "utf-8"
+      vim.o.background = "dark"
+
+      vim.wo.relativenumber = true
+
+
+      require("config.lazy")
+      require("config.lsp")
+    '';
+  };
+}
