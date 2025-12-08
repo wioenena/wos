d75ed1aca684ac4bc1ff4aef2fa1c038f620f359
wioenena <wioenena>
2025-12-08 22:28:54 +0300

    .

--- modules/nixos/programs/default.nix
+++ modules/nixos/programs/default.nix
@@ -2,12 +2,14 @@
 {
   imports = [
     ./dconf.nix
-    ./editors.nix
     ./fish.nix
     ./home-manager.nix
     ./nix-ld.nix
     ./tools.nix
     ./git.nix
     ./steam.nix
+    ./nano.nix
+    ./vim.nix
+    .neovim.nix
   ];
 }
---
+++ modules/nixos/programs/nano.nix
@@ -0,0 +1,4 @@
+{ ... }:
+{
+  programs.nano.enable = true;
+}
