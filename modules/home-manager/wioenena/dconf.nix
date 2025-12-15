9246d029a918e1f55e3917ae5761814b375b228a
wioenena <wioenena>
2025-12-15 15:15:02 +0300

    .

--- modules/home-manager/wioenena/dconf.nix
+++ modules/home-manager/wioenena/dconf.nix
@@ -6,6 +6,41 @@
       "org/gnome/desktop/interface" = {
         color-scheme = "prefer-dark";
       };
+      "org/gnome/shell" = {
+        favorite-apps = [
+          "org.gnome.Settings.desktop"
+          "org.gnome.Nautilus.desktop"
+          "zen-beta.desktop"
+          "org.gnome.Console.desktop"
+          "dev.zed.Zed.desktop"
+          "code.desktop"
+        ];
+      };
+      "org/gnome/shell/extensions/blur-my-shell/applications" = {
+        blur = true;
+        dynamic-opacity = false;
+        whitelist = [ "org.gnome.Console" ];
+      };
+      "org/gnome/shell/extensions/clipboard-indicator" = {
+        notify-on-copy = true;
+        clear-on-boot = true;
+      };
+      "org/gnome/shell/extensions/dash-to-dock" = {
+        dash-max-icon-size = 40;
+        custom-theme-shrink = true;
+      };
+      "org/gnome/shell/extensions/gsconnect" = {
+        enabled = false;
+      };
+      "org/gnome/shell/extensions/lockkeys" = {
+        capslock-notification = "compact";
+        capslock-indicator = "when-active";
+        numlock-notification = "compact";
+        numlock-indicator = "when-active";
+      };
+      "org/gnome/shell/extensions/vitals" = {
+        icon-style = 1;
+      };
     };
   };
 }
--- modules/nixos/ui/gnome.nix
+++ modules/nixos/ui/gnome.nix
@@ -51,5 +51,6 @@
     gnome-tour
     epiphany
     geary
+    devhelp
   ];
 }
