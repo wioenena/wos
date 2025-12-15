0b0c9ee2cf28e4d9b804805dd3aebd6679684796
wioenena <wioenena>
2025-12-15 14:02:11 +0300

    .

--- overlays/gnomeExtensions/default.nix
+++ overlays/gnomeExtensions/default.nix
@@ -1,6 +1,5 @@
 final: prev: {
   gnomeExtensions = prev.gnomeExtensions // {
     lockkeys-vaina = final.callPackage ./lockkeys-vaina.nix { };
-    gtk4-desktop-icons-ng-ding = final.callPackage ./gtk4-desktop-icons-ng-ding.nix { inherit final prev; };
   };
 }
