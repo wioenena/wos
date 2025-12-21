fa44efcd54344b4693f7a79069e7a31af6f7f9d5
wioenena <wioenena>
2025-12-22 00:55:53 +0300

    .

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,7 +1,23 @@
 { ... }:
 {
   imports = [
-    ./system
+    ./amdgpu.nix
+    ./audio.nix
+    ./boot.nix
+    ./documentation.nix
+    ./firmware.nix
+    ./fonts.nix
+    ./fs.nix
+    ./hardware.nix
+    ./locale.nix
+    ./networking.nix
+    ./nix.nix
+    ./packages.nix
+    ./time.nix
+    ./users.nix
+    ./virtualisation.nix
+    ./xdg.nix
+    ./zram.nix
     ./ui
     ./programs
   ];
