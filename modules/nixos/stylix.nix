586918438c3289fa4e1214c2448868806541f917
wioenena <wioenena>
2025-12-23 03:39:41 +0300

    .

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -2,7 +2,13 @@
 {
   stylix = {
     enable = true;
+    enableReleaseChecks = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     polarity = "dark";
+    cursor = {
+      name = "Bibata-Modern-Ice";
+      package = pkgs.bibata-cursors;
+      size = 24;
+    };
   };
 }
