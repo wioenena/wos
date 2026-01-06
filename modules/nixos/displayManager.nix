4df2f3bf99c69fa894cd77e3e2fc592e788783d3
wioenena <wioenena@gmail.com>
2026-01-06 19:46:55 +0300

    .

--- modules/nixos/displayManager.nix
+++ modules/nixos/displayManager.nix
@@ -1,12 +1,4 @@
 { pkgs, ... }:
 {
-  services.displayManager.gdm = {
-    enable = true;
-    # wayland.enable = true;
-    # theme = "${
-    #   pkgs.sddm-astronaut.override {
-    #     embeddedTheme = "japanese_aesthetic";
-    #   }
-    # }/share/sddm/themes/sddm-astronaut-theme";
-  };
+  services.displayManager.gdm.enable = true;
 }
